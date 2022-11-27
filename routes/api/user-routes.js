85af0ad97904eac550720176d1000f8a5d5fe315
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-11-27 11:29:19 -0500

    single users route for get,update, and delete

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -10,10 +10,16 @@ const {
     removeFriend,
 } = require('../../controllers/user-controller');
 
-// Route = /api/users
+// Users Route = /api/users
 
 router.route('/')
   .get(getUsers)
   .post(createUser);
+  
 
+// Single User Route = /api/users/:userId
 
+router.route('/:userId')
+   .get(getSingleUser)
+   .put(updateUser)
+   .delete(deleteUser);
