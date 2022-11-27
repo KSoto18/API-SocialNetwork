64db1ee287d59527df49d9874aa41d83b930e617
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-11-27 11:38:19 -0500

    added starter code to user-controllers

---
+++ controllers/user-controller.js
@@ -0,0 +1,35 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+
+    getUsers(req,res) {
+
+    },
+
+    getSingleUser(req, res) {
+
+    },
+
+    createUser(req, res) {
+
+    },
+
+    updateUser(req, res) {
+
+    },
+
+    deleteUser(req, res) {
+
+    },
+
+    addFriend(req, res) {
+
+    },
+
+    removeFriend(req, res) {
+
+    }
+
+};
+
+module.exports = userController;
