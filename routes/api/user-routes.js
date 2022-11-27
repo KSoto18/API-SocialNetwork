7a16af34f7494b56acff19e06398aab43d4659c5
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-11-27 11:32:03 -0500

    add and remove friend route for user

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -25,7 +25,7 @@ router.route('/:userId')
    .delete(deleteUser);
 
 
-// Route = /api/users/:userId/friends/:friendId
+// Friends Route = /api/users/:userId/friends/:friendId
 
 router.route('/:userId/friends/:friendId')
    .post(addFriend)
