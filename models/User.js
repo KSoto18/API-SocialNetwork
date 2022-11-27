02488301538b7519110687ac1b159a1db2ba0bdb
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-11-27 11:49:15 -0500

    added to user model

--- models/User.js
+++ models/User.js
@@ -3,22 +3,29 @@ const { Schema, model } = require('mongoose');
 const userSchema = new Schema(
     {
         username: {
-
+           type: String,
+           unique: true,
+           required: true,
+           trim: true
         },
 
         email: {
-
+            type: String,
+            unique: true,
+            required: true,
+            match: /^([a-z0-9._-]+)@([a-z0-9.]+)$/gm,
         },
 
         thoughts: [
             {
-
+              type: Schema.Types.ObjectId,
+              ref: 'thought'
             },
         ],
 
         friends: [
             {
-
+             type: Schema.Types.ObjectId
             },
         ],
     },
