b2b0d2ba152a3321bd980bf98d8e9e95f81bf68b
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-11-27 12:31:19 -0500

    added to server.js

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,27 @@
+const router = require('express'),Router();
+
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+} = require('../../controllers/thought-controller');
+
+// Thought Route = /api/thoughts
+
+router.route('/')
+  .get(getThought)
+  .post(createThought);
+
+
+// Single Thought Route = /api/thoughts/
+//needs work
+router.route('/')
+   .get(getSingleThought)
+   .put(updateThought)
+   .delete(deleteThought);
+
+
+
+module.exports = router;
